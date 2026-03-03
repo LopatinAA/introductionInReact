916806724179c9141366aeceed8ac68bae990540
Andrey L <lopatkin.andrushka@yandex.ru>
2026-03-03 12:24:34 +0300

    save change

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,40 @@
+import { useState } from "react"
+
+const arrButtonsOfCalculator = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '=', 'C'
+]
+
+
+
 const App = () => {
+  const [value, setValue] = useState('')
+  const onClick = (props) => {
+    let symbol = props.target.textContent
+    if(symbol == 'C') {
+      setValue('')
+    }
+    if(symbol == '-' || symbol == '+') {
+      
+      
+    }
+    else {
+       // console.log(props.target.textContent)
+    setValue(value + symbol)
+    }
+   
+    
+  }
   return (
     <>
+      <div className='container'>
+        <div className='inputField'>
+        <input value={value}></input>
+        </div>
+        {arrButtonsOfCalculator.map((e) => {
+          return <button key={e} onClick={onClick}>{e}</button>
+        })}
+      </div>
+
     </>
   )
 }
